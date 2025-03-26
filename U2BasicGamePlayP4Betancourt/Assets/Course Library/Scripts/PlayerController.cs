080e31e7ddc8f4047380fdb61e5f14e4ac27107b
TheMicMan <750128@student.dusd.net>
2025-03-26 11:41:07 -0700

    Almost finished the assignment
    Almost done

--- U2BasicGamePlayP4Betancourt/Assets/Course Library/Scripts/PlayerController.cs
+++ U2BasicGamePlayP4Betancourt/Assets/Course Library/Scripts/PlayerController.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 10;
+
+    public GameObject projectilePrefab;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,21 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation); ;
+        }
+        // Keep the player in bounds
+        if (transform.position.x < -xRange) 
+        { 
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange) 
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
 }
