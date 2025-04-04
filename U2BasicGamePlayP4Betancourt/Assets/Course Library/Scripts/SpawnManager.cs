c909e45e44655221abc25b3016f75f24f5b6ea73
TheMicMan <750128@student.dusd.net>
2025-04-04 11:44:19 -0700

    almost done
    about to finish

--- U2BasicGamePlayP4Betancourt/Assets/Course Library/Scripts/SpawnManager.cs
+++ U2BasicGamePlayP4Betancourt/Assets/Course Library/Scripts/SpawnManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +17,21 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            SpawnRandomAnimal();
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX),
+            0, spawnPosZ);
+            Instantiate(animalPrefabs[animalIndex], spawnPos,
+            animalPrefabs[animalIndex].transform.rotation);
+        }
+    }
+
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnpos = new Vector3(-xSpawnRange, xSpawnRange), 0, zSpawnPos);
+        Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20) spawnpos,
+            animalPrefabs[animalIndex].transform.rotation);
     }
 }
