4ef12e0d91b07bb145a70871e25ecf8e598928fd
TheMicMan <750128@student.dusd.net>
2025-04-08 11:55:31 -0700

    Finsihed
    did everything

--- U2BasicGamePlayP4Betancourt/Assets/Course Library/Scripts/PlayerController.cs
+++ U2BasicGamePlayP4Betancourt/Assets/Course Library/Scripts/PlayerController.cs
@@ -10,6 +10,11 @@ public class PlayerController : MonoBehaviour
 
     public GameObject projectilePrefab;
 
+    public float zMin;
+    public float zMax;
+    public float verticalInput;
+    public Transform projectileSpawnPoint;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +24,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Instantiate(projectilePrefab, projectileSpawnPoint.position, projectilePrefab.transform.rotation);
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             // Launch a projectile from the player
@@ -35,5 +42,16 @@ public class PlayerController : MonoBehaviour
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        
+        verticalInput = Input.GetAxis("Vertical"); 
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+        if (transform.position.z < zMin) 
+        { 
+            transform.position = new Vector3(transform.position.x, transform.position.y, zMin); 
+        }
+        if (transform.position.z > zMax) 
+        { 
+            transform.position = new Vector3(transform.position.x, transform.position.y, zMax); 
+        }
     }
 }
--- U2BasicGamePlayP4Betancourt/Assets/Course Library/Scripts/SpawnManager.cs
+++ U2BasicGamePlayP4Betancourt/Assets/Course Library/Scripts/SpawnManager.cs
@@ -5,33 +5,46 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
-    private float spawnRangeX = 20;
-    private float spawnPosZ = 20;
+    private float SpawnRangeX = 20;
+    private float SpawnPosZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
+
+    public float sideSpawnMinZ; 
+    public float sideSpawnMaxZ; 
+    public float sideSpawnX;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            SpawnRandomAnimal();
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX),
-            0, spawnPosZ);
-            Instantiate(animalPrefabs[animalIndex], spawnPos,
-            animalPrefabs[animalIndex].transform.rotation);
-        }
+       
     }
 
     void SpawnRandomAnimal()
     {
         int animalIndex = Random.Range(0, animalPrefabs.Length);
-        Vector3 spawnpos = new Vector3(-xSpawnRange, xSpawnRange), 0, zSpawnPos);
-        Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20) spawnpos,
-            animalPrefabs[animalIndex].transform.rotation);
+        Vector3 spawnPos = new Vector3(Random.Range(-SpawnRangeX, SpawnRangeX), 0, SpawnPosZ);
+        
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+    }
+    void SpawnLeftAnimal() 
+    { 
+        int animalIndex = Random.Range(0, animalPrefabs.Length); 
+        Vector3 spawnPos = new Vector3(-sideSpawnX, 0, Random.Range(sideSpawnMinZ, sideSpawnMaxZ));
+        Vector3 rotation = new Vector3(0, 90, 0);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, Quaternion.Euler(rotation)); 
+    }
+    void SpawnRightAnimal() 
+    { 
+        int animalIndex = Random.Range(0, animalPrefabs.Length); 
+        Vector3 spawnPos = new Vector3(sideSpawnX, 0, Random.Range(sideSpawnMinZ, sideSpawnMaxZ));
+        Vector3 rotation = new Vector3(0, -90, 0);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, Quaternion.Euler(rotation)); 
     }
 }
